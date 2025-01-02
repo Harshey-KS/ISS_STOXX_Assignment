e3c59e2be305c408cbcafeeb9094da2b0cfe5846
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:07:38 +0530

    Updated

--- Java/basics/ClassObjectExample.java
+++ Java/basics/ClassObjectExample.java
@@ -1,25 +1,41 @@
 package Java.src.basics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class Car {
     // Instance variables (state)
-    String brand;
-    int speed;
+    private String brand;
+    private int speed;
+
+    // Logger instance
+    private static final Logger logger = LoggerFactory.getLogger(Car.class);
 
     // Constructor
     public Car(String brand, int speed) {
         this.brand = brand;
         this.speed = speed;
+        logger.info("Car object created with brand: {} and speed: {} km/h", brand, speed);
     }
 
     // Method (behavior)
     public void drive() {
-        System.out.println(brand + " is driving at " + speed + " km/h.");
+        logger.info("{} is driving at {} km/h.", brand, speed);
     }
 }
 
 public class ClassObjectExample {
+    private static final Logger logger = LoggerFactory.getLogger(ClassObjectExample.class);
+
     public static void main(String[] args) {
+        logger.info("Program started");
+
+        // Create a Car object
         Car car = new Car("Toyota", 120);
+
+        // Call the drive method
         car.drive();
+
+        logger.info("Program ended");
     }
 }
