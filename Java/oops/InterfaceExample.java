4a42fe12c0771476665f1ad60380938de68251f1
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:18:36 +0530

    Updated

--- Java/oops/InterfaceExample.java
+++ Java/oops/InterfaceExample.java
@@ -1,18 +1,28 @@
 package Java.src.oops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 interface Animal {
     void sound();
 }
 
 class Cat implements Animal {
+    private static final Logger logger = LoggerFactory.getLogger(Cat.class);
+
+    @Override
     public void sound() {
-        System.out.println("Cat meows");
+        logger.info("Cat meows");
     }
 }
 
 public class InterfaceExample {
+    private static final Logger logger = LoggerFactory.getLogger(InterfaceExample.class);
+
     public static void main(String[] args) {
+        logger.info("Starting Interface Example");
         Animal cat = new Cat();
         cat.sound();
+        logger.info("Finished Example");
     }
 }
