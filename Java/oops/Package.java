2612eb292f54e3cdb59897892a85540ced6fb592
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:20:54 +0530

    Updated

--- Java/oops/Package.java
+++ Java/oops/Package.java
@@ -1,10 +1,14 @@
 package Java.src.oops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import Java.src.basics.ClassObjectExample; // Importing a class from another package
 
 public class Package {
+    private static final Logger logger = LoggerFactory.getLogger(Package.class);
+
     public static void main(String[] args) {
-        System.out.println("Package example with imports");
+        logger.info("Package example with imports");
         ClassObjectExample.main(null); // Accessing main method of another class
     }
 }
