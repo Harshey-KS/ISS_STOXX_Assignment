03102904913d6d5888547e8f82c8ddc8f7839e24
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:17:56 +0530

    Updated

--- Java/oops/AbstractionAndEncapsulation.java
+++ Java/oops/AbstractionAndEncapsulation.java
@@ -1,28 +1,39 @@
 package Java.src.oops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 abstract class Vehicle {
     public abstract void move();
 }
 
 class Bike extends Vehicle {
+    private static final Logger logger = LoggerFactory.getLogger(Bike.class);
+
     @Override
     public void move() {
-        System.out.println("Bike is moving");
+        logger.info("Bike is moving");
     }
 }
 
 class Car extends Vehicle {
+    private static final Logger logger = LoggerFactory.getLogger(Car.class);
+
     @Override
     public void move() {
-        System.out.println("Car is moving");
+        logger.info("Car is moving");
     }
 }
 
 public class AbstractionAndEncapsulation {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractionAndEncapsulation.class);
+
     public static void main(String[] args) {
+        logger.info("Starting Abstraction and Encapsulation Example");
         Vehicle bike = new Bike();
         Vehicle car = new Car();
         bike.move();
         car.move();
+        logger.info("Finished Example");
     }
 }
