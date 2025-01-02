6e47aa0dbce770b4e44aedf37257ca0599c9d89b
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:09:27 +0530

    Updated

--- Java/basics/LoopExample.java
+++ Java/basics/LoopExample.java
@@ -1,34 +1,39 @@
 package Java.src.basics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LoopExample {
+    private static final Logger logger = LoggerFactory.getLogger(LoopExample.class);
+
     public static void main(String[] args) {
         // Decision making
         int number = 10;
         if (number > 0) {
-            System.out.println("Positive number");
+            logger.info("Positive number");
         } else {
-            System.out.println("Negative number");
+            logger.info("Negative number");
         }
 
         // For loop
         for (int i = 0; i < 5; i++) {
-            System.out.println("For loop: " + i);
+            logger.info("For loop: {}", i);
         }
 
         // While loop
         int j = 0;
         while (j < 5) {
-            System.out.println("While loop: " + j);
+            logger.info("While loop: {}", j);
             j++;
         }
 
         // Switch case
         int day = 3;
         switch (day) {
-            case 1: System.out.println("Monday"); break;
-            case 2: System.out.println("Tuesday"); break;
-            case 3: System.out.println("Wednesday"); break;
-            default: System.out.println("Invalid day");
+            case 1: logger.info("Monday"); break;
+            case 2: logger.info("Tuesday"); break;
+            case 3: logger.info("Wednesday"); break;
+            default: logger.info("Invalid day");
         }
     }
 }
