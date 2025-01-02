e4bb8d042867cfb67aaaf3a20d9ed495f8253041
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:18:13 +0530

    Updated

--- Java/oops/InheritanceExample.java
+++ Java/oops/InheritanceExample.java
@@ -1,21 +1,32 @@
 package Java.src.oops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class Animal {
+    private static final Logger logger = LoggerFactory.getLogger(Animal.class);
+
     public void sound() {
-        System.out.println("Animal makes a sound");
+        logger.info("Animal makes a sound");
     }
 }
 
 class Dog extends Animal {
+    private static final Logger logger = LoggerFactory.getLogger(Dog.class);
+
     @Override
     public void sound() {
-        System.out.println("Dog barks");
+        logger.info("Dog barks");
     }
 }
 
 public class InheritanceExample {
+    private static final Logger logger = LoggerFactory.getLogger(InheritanceExample.class);
+
     public static void main(String[] args) {
+        logger.info("Starting Inheritance Example");
         Animal animal = new Dog();
         animal.sound();
+        logger.info("Finished Example");
     }
 }
