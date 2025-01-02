0fb7626de3bef36cf2a4c5f5e9298d9a03952f0c
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:12:26 +0530

    Updated

--- Java/collections/Collection.java
+++ Java/collections/Collection.java
@@ -1,24 +1,28 @@
 package Java.collections;
 
 import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Collection {
+    private static final Logger logger = LoggerFactory.getLogger(Collection.class);
+
     public static void main(String[] args) {
         // List Example
         List<String> list = new ArrayList<>();
         list.add("Apple");
         list.add("Banana");
-        System.out.println("List: " + list);
+        logger.info("List: {}", list);
 
         // Set Example
         Set<String> set = new HashSet<>(list);
         set.add("Cherry");
-        System.out.println("Set: " + set);
+        logger.info("Set: {}", set);
 
         // Map Example
         Map<Integer, String> map = new HashMap<>();
         map.put(1, "One");
         map.put(2, "Two");
-        System.out.println("Map: " + map);
+        logger.info("Map: {}", map);
     }
 }
