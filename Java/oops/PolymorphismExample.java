33d3b247127ec253e54878d913e893f7970bd6c9
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:21:38 +0530

    Updated

--- Java/oops/PolymorphismExample.java
+++ Java/oops/PolymorphismExample.java
@@ -1,29 +1,44 @@
 package Java.src.oops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class Shape {
+    private static final Logger logger = LoggerFactory.getLogger(Shape.class);
+
     public void draw() {
-        System.out.println("Drawing a shape");
+        logger.info("Drawing a shape");
     }
 }
 
 class Circle extends Shape {
+    private static final Logger logger = LoggerFactory.getLogger(Circle.class);
+
+    @Override
     public void draw() {
-        System.out.println("Drawing a circle");
+        logger.info("Drawing a circle");
     }
 }
 
 class Rectangle extends Shape {
+    private static final Logger logger = LoggerFactory.getLogger(Rectangle.class);
+
+    @Override
     public void draw() {
-        System.out.println("Drawing a rectangle");
+        logger.info("Drawing a rectangle");
     }
 }
 
 public class PolymorphismExample {
+    private static final Logger logger = LoggerFactory.getLogger(PolymorphismExample.class);
+
     public static void main(String[] args) {
+        logger.info("Starting Polymorphism Example");
         Shape shape1 = new Circle();
         Shape shape2 = new Rectangle();
 
         shape1.draw();
         shape2.draw();
+        logger.info("Finished Polymorphism Example");
     }
 }
