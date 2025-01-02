0ea5cc0ee69b810221c56b7542598e5eab342823
Harshey-KS <138758165+Harshey-KS@users.noreply.github.com>
2025-01-02 22:10:11 +0530

    Updated

--- Java/basics/StringArrayExample.java
+++ Java/basics/StringArrayExample.java
@@ -1,16 +1,21 @@
 package Java.src.basics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StringArrayExample {
+    private static final Logger logger = LoggerFactory.getLogger(StringArrayExample.class);
+
     public static void main(String[] args) {
         // String operations
         String text = "Hello, World!";
-        System.out.println("Length: " + text.length());
-        System.out.println("Substring: " + text.substring(0, 5));
+        logger.info("Length: {}", text.length());
+        logger.info("Substring: {}", text.substring(0, 5));
 
         // Array operations
         int[] numbers = {1, 2, 3, 4, 5};
         for (int num : numbers) {
-            System.out.println("Number: " + num);
+            logger.info("Number: {}", num);
         }
     }
 }
